6436a0cad873a4936066d2b24f6b9cd6979e4ac6
Matthew Jones <matts3371@gmail.com>
2020-04-06 23:35:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,9 @@ class PostsController < ApplicationController
     @post.update(post_params)
     
     if @post.valid? 
-      @post.save 
       redirect_to post_path(@post)
     else 
-      render :new 
+      render :edit 
     end 
   end
 
